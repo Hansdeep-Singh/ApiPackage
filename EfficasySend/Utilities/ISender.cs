ae981083f86f55401ebde8b390287e3b37f72f32
Hansdeep Singh <hansdeep.singh@hotmail.com>
2022-08-04 11:40:41 +1000

    Initial

---
+++ EfficasySend/Utilities/ISender.cs
@@ -0,0 +1,10 @@
+﻿using EfficacySend.BluePrint;
+using System.Threading.Tasks;
+
+namespace EfficacySend.Utilities
+{
+    public interface ISender
+    {
+        Task<SendEmailResponse> SendEmailAll(Email se);
+    }
+}
