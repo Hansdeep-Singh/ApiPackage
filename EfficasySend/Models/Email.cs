88e94af830834464b08a63b769c4d5a276274c52
Hansdeep Singh <hansdeep.singh@hotmail.com>
2022-08-04 12:34:18 +1000

    wip

--- EfficasySend/Models/Email.cs
+++ EfficasySend/Models/Email.cs
@@ -9,13 +9,13 @@ namespace EfficacySend.BluePrint
 {
    public class Email
     {
-        public string FromEmail { get; set; }
-        public string FromName { get; set; }
-        public string ToEmail { get; set; }
-        public string ToName { get; set; }
-        public string Subject { get; set; }
-        public string PlainEmail { get; set; }
-        public string HtmlEmail { get; set; }
+        public string? FromEmail { get; set; }
+        public string? FromName { get; set; }
+        public string? ToEmail { get; set; }
+        public string? ToName { get; set; }
+        public string? Subject { get; set; }
+        public string? PlainEmail { get; set; }
+        public string HtmlEmail { get; set; } = string.Empty;
     }
     public class SendEmailResponse
     {
---
+++ GoogleOAuth/UriService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Web.Services3.Security;
+
+namespace GoogleOAuth
+{
+    public class UriService
+    {
+        public static string GetAuthUri(string clientId, string redirectUri)
+        {
+            Nonce nonce = new(32);
+            var query = new QueryBuilder();
+            query.Add("response_type", "code");
+            query.Add("client_id", clientId);
+            query.Add("scope", "openid profile email");
+            query.Add("redirect_uri", redirectUri);
+            return query.ToString();
+        }
+
+        public static string GetAccessUri(string clientId, string code, string secret, string redirectUri)
+        {
+
+            var query = new QueryBuilder();
+            query.Add("redirect_uri", redirectUri);
+            string queryString = query.ToString();
+            string queryStringNoQuestion = queryString.Replace("?", "");
+
+            string url = $"https://oauth2.googleapis.com/token?code={code}&client_id={clientId}&client_secret={secret}&{queryStringNoQuestion}&grant_type=authorization_code";
+
+
+            return url;
+        }
+    }
+}
