104a66ad649d1af2ceac2d2f1b5b7a7186acb716
Hansdeep Singh <hansdeep.singh@hotmail.com>
2022-08-05 11:11:48 +1000

    wip

---
+++ HttpCall/CallService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Net.Http.Headers;
+
+namespace HttpCall
+{
+    public class CallService
+    {
+
+        private readonly IHttpClientFactory httpClientFactory;
+
+
+        public CallService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> HttpGetCall<T>(string URI, string contentType)
+        {
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, URI)
+            {
+                Headers =  {
+                { HeaderNames.Accept, contentType }
+            }
+            };
+            var httpClient = httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            var response = await httpResponseMessage.Content.ReadAsAsync<T>();
+            return response;
+        }
+
+        public async Task<T> HttpPostCall<T>(string URI, string contentType)
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, URI)
+            {
+                Headers =  {
+                { HeaderNames.Accept, contentType }
+            }
+            };
+            var httpClient = httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            var response = await httpResponseMessage.Content.ReadAsAsync<T>();
+            return response;
+        }
+
+        public async Task<T> HttpCallNew<T>(string URI, string contentType)
+        {
+
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, URI)
+            {
+                Headers =  {
+                { HeaderNames.ContentType, contentType }
+            }
+            };
+            var httpClient = httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            var response = await httpResponseMessage.Content.ReadAsAsync<T>();
+            return response;
+        }
+
+
+
+    }
+}
---
+++ Logic/Hashing.cs
@@ -0,0 +1,16 @@
+﻿using BC = BCrypt.Net.BCrypt;
+namespace Logic
+{
+    public class Hashing
+    {
+        public string PasswordHash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+        public bool PasswordVerify(string pass, string dbpass)
+        {
+            return BC.Verify(pass, dbpass);
+        }
+
+    }
+}
