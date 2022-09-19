0702748e3c913ca5649001f4a2c24ec0cc1a45d6
Hansdeep Singh <hansdeep.singh@hotmail.com>
2022-09-19 10:19:02 +1000

    wip

--- ApiWeb/Constants/AppConsts.cs
+++ ApiWeb/Constants/AppConsts.cs
@@ -2,7 +2,7 @@
 {
     public static class AppConsts
     {
-        public static readonly string[] CORSOrigins = { "http://localhost:4200", "http://www.hansdeep.com", "http://hansdeep.com" };
+        public static readonly string[] CORSOrigins = { "http://localhost:4200", "http://www.hansdeep.com", "http://hansdeep.com", "http://127.0.0.1:5500" };
         
         public enum TokenType
         {
